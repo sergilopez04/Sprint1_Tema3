3aaaf88704af6ed13688095f5f6551f4132088ca
[Sergi López] <[socelsergi@gmail.com]>
2024-05-14 15:54:48 +0200

    Ex4 fet

---
+++ Ex4.php
@@ -0,0 +1,12 @@
+<?php
+
+$dataPeople = [
+    "Sergi" => [
+        "edad" => 22,
+        "email" => "socelsergi@gmail.com",
+        "menjarIdeal" => "Focaccia"
+    ]
+    ];
+
+    var_dump($dataPeople);
+?>
